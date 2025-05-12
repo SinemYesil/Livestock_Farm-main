8f6e5d43f89d5c07472e6421c5f76075723be3f4
SinemYesil <105049945+SinemYesil@users.noreply.github.com>
2025-05-12 22:15:12 +0300

    Update FeedFactory.java
    cattle factory yi kaldırdım

--- src/FeedFactory.java
+++ src/FeedFactory.java
@@ -37,16 +37,4 @@ class BeefFeedFactory implements FeedFactory {
     public Protein createProtein() { return new Canola(); }
 }
 
-class CattleFactory {
-    public static Cattle createCattle(String type, String id, String location) {
-        switch (type.toLowerCase()) {
-            case "dairy":
-                return new DairyCattle(id, location);
-            case "beef":
-                return new BeefCattle(id, location);
-            default:
-                throw new IllegalArgumentException("Unknown cattle type: " + type);
-        }
-    }
-}
 
